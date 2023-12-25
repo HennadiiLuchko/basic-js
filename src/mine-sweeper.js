4d4714befac23dca60e9a0be7d227c4127599e5e
HennadiiLuchko <137803124+HennadiiLuchko@users.noreply.github.com>
2023-12-25 13:56:13 +0200

    feat: add tasks solutions

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,67 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+
+  if (!matrix || matrix.length === 0 || matrix[0].length === 0) {
+    return [];
+  };
+
+  // const res = [];
+  // for(let i = 0; i < matrix.length; i++) {
+  //   let rowRes = [];
+  //   for(let j = 0; j < matrix[i].length; j++) {
+  //     let count = 0;
+  //     if(matrix[i][j-1]) count++;
+  //     if(matrix[i][j+1]) count++;
+
+  //     if(i-1 >= 0) {
+  //       if(matrix[i-1][j-1]) count++;
+  //       if(matrix[i-1][j]) count++;
+  //       if(matrix[i-1][j+1]) count++;
+  //     }
+  //     if(i+1 < matrix.length) {
+  //       if(matrix[i+1][j-1]) count++;
+  //       if(matrix[i+1][j]) count++;
+  //       if(matrix[i+1][j+1]) count++;
+  //     }
+
+  //     rowRes.push(count);
+  //   }
+
+  //   res.push(rowRes)
+  //   rowRes = [];
+
+  // }
+
+  // return res;
+
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const res = [];
+
+  for (let i = 0; i < rows; i++) {
+    const rowRes = [];
+
+    for (let j = 0; j < cols; j++) {
+      let count = 0;
+
+      for (let k = i - 1; k <= i + 1; k++) {
+        for (let l = j - 1; l <= j + 1; l++) {
+          if (k >= 0 && l >= 0 && k < rows &&  l < cols && !(k === i && l === j)) {
+            if (matrix[k][l]) {
+              count++;
+            }
+          }
+        }
+      }
+      rowRes.push(count);
+    }
+
+    res.push(rowRes);
+  }
+
+  return res;
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  getVigenereCipheringMachineCrypt(message, key, isDirect) {
+
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    if (message.length > key.length) {
+      key = key.repeat(Math.ceil(message.length / key.length));
+    }
+
+    const inputMessage = message.toUpperCase();
+    const inputKey = key.toUpperCase();
+    
+    let res = '';
+
+    for (let i = 0, j = 0; i < inputMessage.length; i++) {
+      let ch = inputMessage[i];
+
+      if (this.alphabet.indexOf(ch) === -1) {
+        res += ch;
+        continue;
+      }
+
+      let keyChar = inputKey[j];
+      let keyIndex = this.alphabet.indexOf(keyChar);
+      let codeIndex;
+
+      if (isDirect) {
+        codeIndex = (this.alphabet.indexOf(ch) + keyIndex) % this.alphabet.length;
+      } else {
+        codeIndex = (this.alphabet.indexOf(ch) - keyIndex + this.alphabet.length) % this.alphabet.length;
+      }
+
+      res += this.alphabet[codeIndex];
+      j++;
+    }
+
+    if(this.isDirect) return res;
+    return res.split('').reverse().join('');
+  }
+
+
+  encrypt(message, key) {
+    return this.getVigenereCipheringMachineCrypt(message, key, true);
+  }
+
+  decrypt(message, key) {
+    return this.getVigenereCipheringMachineCrypt(message, key, false);
   }
 }
 
